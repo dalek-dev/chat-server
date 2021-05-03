befae7acec1a7d35935a22cced9403a11cb05de5
victwise <victor.m.diniz@gmail.com>
2021-05-03 12:32:47 -0500

    added jwt

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,29 +1,39 @@
 const jwt = require('jsonwebtoken');
 
-const validatJwt = (req, res, next) => {
+const validarJWT = ( req, res, next ) => {
+
+    // Leer token
     const token = req.header('x-token');
 
-    if(!token){
+    if ( !token ) {
         return res.status(401).json({
-            ok:false,
+            ok: false,
             msg: 'No hay token en la petición'
         });
     }
 
-    try{
-        const { uid } = jwt.verify(token, process.env.JWT_KEY);
+    try {
+
+        const { uid } = jwt.verify( token, process.env.JWT_KEY );
         req.uid = uid;
+        
         next();
-    }catch(error){
-        console.log(error);
+
+    } catch (error) {
         return res.status(401).json({
-            ok:false,
+            ok: false,
             msg: 'Token no válido'
-        });
+        })
     }
 
+
+
+
 }
 
+
 module.exports = {
-    validatJwt
+    validarJWT
 }
+
+
